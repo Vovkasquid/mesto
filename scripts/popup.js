270d31ec5e3a4240b909ac481be2cceec7aa7609
Vladimir Blinov <zemetrans21@yandex.ru>
2021-02-25 02:22:13 +0300

    fix: исправить отступы в js + ошибка в profile__description

--- scripts/popup.js
+++ scripts/popup.js
@@ -7,20 +7,20 @@ let profileDescription = document.querySelector('.profile__description');
 
 //Пишем обработчики, которые потом будут применяться при кликах на кнопки
 function openPopupHandler () {
-    document.querySelector('.edit-form__profile-name').value = document.querySelector('.profile__name').textContent;
-    document.querySelector('.edit-form__profile-description').value = document.querySelector('.profile__description').textContent;
-    editForm.classList.add('edit-form_status_active');
+  document.querySelector('.edit-form__profile-name').value = document.      querySelector('.profile__name').textContent;
+  document.querySelector('.edit-form__profile-description').value = document.querySelector('.profile__description').textContent;
+  editForm.classList.add('edit-form_status_active');
 }
 
 function closePopupHandler () {
-    editForm.classList.remove('edit-form_status_active');
+  editForm.classList.remove('edit-form_status_active');
 }
 
 function editFormSubmitHandler (event) {
-    event.preventDefault();
-    profileName.textContent = document.querySelector('.edit-form__profile-name').value;
-    profileDescription.textContent = document.querySelector('.edit-form__profile-description').value;
-    closePopupHandler();
+  event.preventDefault();
+  profileName.textContent = document.querySelector('.edit-form__profile-name').value;
+  profileDescription.textContent = document.querySelector('.edit-form__profile-description').value;
+  closePopupHandler();
 }
 
 //Вешаем слушателей на кнопки и форму
