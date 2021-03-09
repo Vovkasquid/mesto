d84495bcf86601d64146c71633b89797238f47af
Vladimir Blinov <zemetrans21@yandex.ru>
2021-03-10 01:03:32 +0300

    feat: сделать анимацию

--- scripts/index.js
+++ scripts/index.js
@@ -51,7 +51,7 @@ const initialCards = [
     link: 'https://sun9-35.userapi.com/impg/5dNmV-IcN0BafK_xK8xv2XT1ulh2NO9YJBTvqQ/dImxq2hEho4.jpg?size=2560x1707&quality=96&sign=28edc76e95462d7a0ac529e9bc21d0f1&type=album'
   },
   {
-    name: 'Крепость орешек',
+    name: 'Крепость Орешек',
     link: 'https://sun9-34.userapi.com/impg/MwRJsBY7hZGQwAcSCgBlVWGkvta2OZEH-IL_Gw/WYba50g-obM.jpg?size=2560x1707&quality=96&sign=0e342caa76313508ec0b0c4d0dd03603&type=album'
   },
   {
