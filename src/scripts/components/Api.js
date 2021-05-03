b399b6c4464b4851f578c3945b0396a5ac417604
Vladimir Blinov <zemetrans21@yandex.ru>
2021-05-03 19:03:40 +0300

    feat: добавить все методы Api, но пока не проверять их

--- src/scripts/components/Api.js
+++ src/scripts/components/Api.js
@@ -39,4 +39,101 @@ export default class Api {
     //Отдаём промис
     return initialCardPromise;
   }
+
+  editProfile(userName, userAbout) {
+    const editProfilePromise = fetch(`${this._baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: userName,
+        about: userAbout
+      })
+    })
+      .then(res => {
+        //Проверяем успешен ли запрос
+        if (res.ok) {
+          return res.json();
+        }
+        //Если условие не выполнено, то делаем промис с ошибкой
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
+    //Отдаём промис
+    return editProfilePromise;
+  }
+
+  editAvatar(userAvatar) {
+    const editAvatarePromise = fetch(`${this._baseUrl}/users/me/avatar `, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: userAvatar
+      })
+    })
+      .then(res => {
+        //Проверяем успешен ли запрос
+        if (res.ok) {
+          return res.json();
+        }
+        //Если условие не выполнено, то делаем промис с ошибкой
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
+    //Отдаём промис
+    return editAvatarePromise;
+  }
+
+  addCard(placeName, pictureLink) {
+    const addCardPromise = fetch(`${this._baseUrl}/cards `, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify({
+        name: placeName,
+        link: pictureLink
+      })
+    })
+      .then(res => {
+        //Проверяем успешен ли запрос
+        if (res.ok) {
+          return res.json();
+        }
+        //Если условие не выполнено, то делаем промис с ошибкой
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
+    //Отдаём промис
+    return addCardPromise;
+  }
+
+  addLike(cardID) {
+    const addLikePromise = fetch(`${this._baseUrl}/cards/likes/${cardID}`, {
+      method: 'PUT',
+      headers: this._headers
+    })
+      .then(res => {
+        //Проверяем успешен ли запрос
+        if (res.ok) {
+          return res.json();
+        }
+        //Если условие не выполнено, то делаем промис с ошибкой
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
+    //Отдаём промис
+    return addLikePromise;
+  }
+
+  removeLike(cardID) {
+    const removeLikePromise = fetch(`${this._baseUrl}/cards/likes/${cardID}`, {
+      method: 'DELETE',
+      headers: this._headers
+    })
+      .then(res => {
+        //Проверяем успешен ли запрос
+        if (res.ok) {
+          return res.json();
+        }
+        //Если условие не выполнено, то делаем промис с ошибкой
+        return Promise.reject(`Ошибка: ${res.status}`);
+      });
+    //Отдаём промис
+    return removeLikePromise;
+  }
+
 }
